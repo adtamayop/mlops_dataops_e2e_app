ee5fe276d90b6136a4edc81a3f3f7322d510ca3d
adtamayop <adtamayop@unal.edu.co>
2021-08-11 15:28:43 +0000

    bash file for dataset creation

---
+++ create_dataset.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# python src/data/get_external_data.py
+python src/data/merge_raw_data.py
+python src/data/labelling.py
+python src/data/preprocess.py
+dvc add data/processed
+dvc add data/raw
+dvc commit 
+git add data/processed.dvc data/raw.dvc
+dvc push
+git commit --no-verify -m "`date` updated dataset"
